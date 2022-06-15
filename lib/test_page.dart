fa093c2511aecdf833fa310c9a901f1fbea4dfaa
najmulmyself <najmul.myself@gmail.com>
2022-06-15 12:15:27 +0600

    checkbox need to be worked"

--- lib/test_page.dart
+++ lib/test_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:radio_button_form_field/radio_button_form_field.dart';
+import 'package:checkbox_formfield/checkbox_formfield.dart';
 
 class TestFormPage extends StatefulWidget {
   TestFormPage({Key? key}) : super(key: key);
@@ -10,7 +11,21 @@ class TestFormPage extends StatefulWidget {
 
 class _TestFormPageState extends State<TestFormPage> {
   final formKey = GlobalKey<FormState>();
-  List person = ['Rahim', 'Karim', 'Hasem', 'Kashem'];
+  bool? checkboxIconFormFieldValue = false;
+  List<Map> person = [
+    {
+      'value': 1,
+      'name': 'Phone',
+    },
+    {
+      'value': 2,
+      'name': 'Desktop',
+    },
+    {
+      'value': 3,
+      'name': 'Laptop',
+    },
+  ];
   String? select;
   @override
   Widget build(BuildContext context) {
@@ -22,14 +37,55 @@ class _TestFormPageState extends State<TestFormPage> {
         key: formKey,
         child: Column(
           children: [
-            Radio(
-                value: person.length,
-                groupValue: select,
-                onChanged: (value) {
-                  setState(() {
-                    select = value.toString();
-                  });
-                })
+            // Radio(
+            //     value: person.length,
+            //     groupValue: select,
+            //     onChanged: (value) {
+            //       setState(() {
+            //         select = value.toString();
+            //       });
+            //     })
+
+            RadioButtonFormField(
+                data: person,
+                value: 'value',
+                display: 'name',
+                context: context),
+            CheckboxListTileFormField(
+              title: Text('Laptop!'),
+              onSaved: (bool? value) {
+                print('This is : $value');
+              },
+              validator: (bool? value) {
+                if (value!) {
+                  return null;
+                } else {
+                  return 'False!';
+                }
+              },
+            ),
+            CheckboxListTileFormField(
+              title: Text('Desktop!'),
+            ),
+            CheckboxListTileFormField(
+              title: Text('Phone!'),
+            ),
+            CheckboxIconFormField(
+              context: context,
+              initialValue: checkboxIconFormFieldValue!,
+              enabled: true,
+              iconSize: 32,
+              onSaved: (bool? value) {
+                checkboxIconFormFieldValue = value;
+              },
+              onChanged: (value) {
+                if (value) {
+                  print("Icon Checked :)");
+                } else {
+                  print("Icon Not Checked :(");
+                }
+              },
+            ),
           ],
         ),
       ),
