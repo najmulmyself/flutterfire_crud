d2288f21d1fefaaf4aaac03586d8d1481cd89b27
najmulmyself <najmul.myself@gmail.com>
2022-06-20 13:15:34 +0600

    added multiple radiobox

--- lib/test_page.dart
+++ lib/test_page.dart
@@ -31,6 +31,20 @@ class _TestFormPageState extends State<TestFormPage> {
       'name': 'Laptop',
     },
   ];
+  List<Map> language = [
+    {
+      'value': 'Dart',
+      'name': 'Dart',
+    },
+    {
+      'value': 'Python',
+      'name': 'Python',
+    },
+    {
+      'value': 'Javascript',
+      'name': 'Javascript',
+    },
+  ];
   String? select;
 
   void onChanged(dynamic val) => print(val.toString());
@@ -42,139 +56,154 @@ class _TestFormPageState extends State<TestFormPage> {
       ),
       body: Form(
         key: formKey,
-        child: Column(
-          children: [
-            // Radio(
-            //     value: person.length,
-            //     groupValue: select,
-            //     onChanged: (value) {
-            //       setState(() {
-            //         select = value.toString();
-            //       });
-            //     })
-            RadioButtonFormField(
-              toggleable: true,
-              data: gadget,
-              value: 'value',
-              display: 'name',
-              context: context,
-              validator: (value) {
-                if (value.toString() == 'null') {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Please Select a value'),
-                    ),
-                  );
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Radio(
+              //     value: person.length,
+              //     groupValue: select,
+              //     onChanged: (value) {
+              //       setState(() {
+              //         select = value.toString();
+              //       });
+              //     })
+              Text('Select A gadget'),
+              RadioButtonFormField(
+                toggleable: true,
+                data: gadget,
+                value: 'value',
+                display: 'name',
+                context: context,
+                validator: (value) {
+                  if (value.toString() == 'null') {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Please Select a value'),
+                      ),
+                    );
+                    return null;
+                  } else {
+                    setState(() {
+                      myRadioValue = value;
+                      print(myRadioValue);
+                    });
+                  }
                   return null;
-                } else {
-                  setState(() {
-                    myRadioValue = value;
-                    print('I am from set state 62 : $myRadioValue');
-                  });
-                }
-                return null;
-              },
-              // onSaved: (value) {
-              //   setState(() {
-              //     myRadioValue = value;
-              //     myRadioValue = value.toString();
-              //     print(myRadioValue);
-              //   });
-              // },
-            ),
-            // CheckboxListTileFormField(
-            //   title: Text('Laptop!'),
-            //   onSaved: (bool? value) {
-            //     print('This is : $value');
-            //   },
-            //   validator: (bool? value) {
-            //     if (value!) {
-            //       return null;
-            //     } else {
-            //       return 'False!';
-            //     }
-            //   },
-            //   onChanged: (value) {
-            //     if (value) {
-            //       print("ListTile Checked :)");
-            //     } else {
-            //       print("ListTile Not Checked :(");
-            //     }
-            //   },
-            // ),
-            // CheckboxListTileFormField(
-            //   title: Text('Desktop!'),
-            // ),
-            // CheckboxListTileFormField(
-            //   title: Text('Phone!'),
-            // ),
-            // CheckboxIconFormField(
-            //   context: context,
-            //   initialValue: checkboxIconFormFieldValue!,
-            //   enabled: true,
-            //   iconSize: 32,
+                },
+              ),
+              Text('Select A Language'),
+              RadioButtonFormField(
+                toggleable: true,
+                data: language,
+                value: 'value',
+                display: 'name',
+                context: context,
+                validator: (value) {
+                  if (value.toString() == 'null') {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Please Select a value'),
+                      ),
+                    );
+                    return null;
+                  } else {
+                    setState(() {
+                      myRadioValue = value;
+                      print(myRadioValue);
+                    });
+                  }
+                  return null;
+                },
+              ),
+              // CheckboxListTileFormField(
+              //   title: Text('Laptop!'),
+              //   onSaved: (bool? value) {
+              //     print('This is : $value');
+              //   },
+              //   validator: (bool? value) {
+              //     if (value!) {
+              //       return null;
+              //     } else {
+              //       return 'False!';
+              //     }
+              //   },
+              //   onChanged: (value) {
+              //     if (value) {
+              //       print("ListTile Checked :)");
+              //     } else {
+              //       print("ListTile Not Checked :(");
+              //     }
+              //   },
+              // ),
+              // CheckboxListTileFormField(
+              //   title: Text('Desktop!'),
+              // ),
+              // CheckboxListTileFormField(
+              //   title: Text('Phone!'),
+              // ),
+              // CheckboxIconFormField(
+              //   context: context,
+              //   initialValue: checkboxIconFormFieldValue!,
+              //   enabled: true,
+              //   iconSize: 32,
 
-            //   // onSaved: (bool? value) {
-            //   //   checkboxIconFormFieldValue = value;
-            //   // },
-            //   // onChanged: (value) {
-            //   //   if (value) {
-            //   //     print("Icon Checked :)");
-            //   //   } else {
-            //   //     print("Icon Not Checked :(");
-            //   //   }
-            //   // },
-            // ),
+              //   // onSaved: (bool? value) {
+              //   //   checkboxIconFormFieldValue = value;
+              //   // },
+              //   // onChanged: (value) {
+              //   //   if (value) {
+              //   //     print("Icon Checked :)");
+              //   //   } else {
+              //   //     print("Icon Not Checked :(");
+              //   //   }
+              //   // },
+              // ),
 
-            // MultiSelectFormField(
-            //   // autovalidate: AutovalidateMode.disabled,
-            //   dataSource: gadget,
-            //   textField: 'display',
-            //   valueField: 'value',
-            //   hintWidget: Text('Please choose one or more'),
-            //   // initialValue: myCheckValue,
-            // ),
+              // MultiSelectFormField(
+              //   // autovalidate: AutovalidateMode.disabled,
+              //   dataSource: gadget,
+              //   textField: 'display',
+              //   valueField: 'value',
+              //   hintWidget: Text('Please choose one or more'),
+              //   // initialValue: myCheckValue,
+              // ),
 
-            FormBuilderCheckboxGroup(
-              name: 'Gadget',
-              orientation: OptionsOrientation.vertical,
-              options: const [
-                FormBuilderFieldOption(value: 'Phone'),
-                FormBuilderFieldOption(value: 'Desktop'),
-                FormBuilderFieldOption(value: 'Laptop'),
-                FormBuilderFieldOption(value: 'Tablet'),
-              ],
-              wrapDirection: Axis.horizontal,
-              // wrapVerticalDirection: VerticalDirection.down,
-              onChanged: onChanged,
-              validator: (value) {
-                if (value.toString() == 'null') {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Please Select a value'),
-                    ),
-                  );
-                  return 'fasdfsdf';
-                } else {
-                  myCheckValue = value.toString();
-                  print(myCheckValue);
-                  
-                }
-              },
-            ),
+              FormBuilderCheckboxGroup(
+                name: 'Gadget',
+                orientation: OptionsOrientation.vertical,
+                options: const [
+                  FormBuilderFieldOption(value: 'Phone'),
+                  FormBuilderFieldOption(value: 'Desktop'),
+                  FormBuilderFieldOption(value: 'Laptop'),
+                  FormBuilderFieldOption(value: 'Tablet'),
+                ],
+                wrapDirection: Axis.horizontal,
+                // wrapVerticalDirection: VerticalDirection.down,
+                onSaved: onChanged,
+                validator: (value) {
+                  if (value.toString() == 'null') {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Please Select a value'),
+                      ),
+                    );
+                    return null;
+                  } else {
+                    myCheckValue = value.toString();
+                    print(myCheckValue);
+                  }
+                },
+              ),
 
-            ElevatedButton(
-              onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  // print(myRadioValue);
-                }
-                // print('I am from onPressed 122: $myRadioValue');
-                print(myCheckValue);
-                final snackBar = SnackBar(content: Text(''));
-              },
-              child: Text('Submit'),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {}
+                  // print(myCheckValue);
+                },
+                child: Text('Submit'),
+              ),
+            ],
+          ),
         ),
       ),
     );
